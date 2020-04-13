63f54f1deb1dd7a1dec52167b99e39f362db089f
Miguel Menendez <miguel@cogitocode.org>
2020-04-13 10:44:56 -0400

    build file

---
+++ client/build/precache-manifest.66f7cb0e3471eea74fe478271ac3eeda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897fa43bdb053440b59c539fa36c57a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f2be96997d0ae3a8fcd",
+    "url": "/static/css/main.7c490104.chunk.css"
+  },
+  {
+    "revision": "bd3e22741422ad7eb0b1",
+    "url": "/static/js/2.cf0864df.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cf0864df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2be96997d0ae3a8fcd",
+    "url": "/static/js/main.17114617.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
